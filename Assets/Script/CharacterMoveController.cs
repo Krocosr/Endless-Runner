ee189a67278a862f50303c7dc4d220a784b11b1f
Krocosr <ariopang@gmail.com>
2022-06-10 18:58:52 +0700

    Add Jumping script
    - add jumping script
    - using raycast and layers

--- Assets/Script/CharacterMoveController.cs
+++ Assets/Script/CharacterMoveController.cs
@@ -8,6 +8,12 @@ public class CharacterMoveController : MonoBehaviour
     public float moveAccel;
     public float maxSpeed;
 
+    [Header("Jump")]
+    public float jumpAccel;
+
+    private bool isJumping;
+    private bool isOnGround;
+
     private Rigidbody2D rig;
 
     void Start()
@@ -15,16 +21,58 @@ public class CharacterMoveController : MonoBehaviour
         rig = GetComponent<Rigidbody2D>();    
     }
 
+    [Header("Ground Raycast")]
+    public float groundRaycastDistance;
+    public LayerMask groundLayerMask;
+
     private void FixedUpdate()
     {
         Vector2 velocityVector = rig.velocity;
         velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime, 0.0f, maxSpeed);
         rig.velocity = velocityVector;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundRaycastDistance, groundLayerMask);
+        if (hit)
+        {
+            if (!isOnGround && rig.velocity.y <= 0)
+            {
+                isOnGround = true;
+            }
+        }
+        else
+        {
+            isOnGround = false;
+        }
+
+        // calculate velocity vector
+        if (isJumping)
+        {
+            velocityVector.y += jumpAccel;
+            isJumping = false;
+        }
+        velocityVector.x = Mathf.Clamp(velocityVector.x + moveAccel * Time.deltaTime,
+        0.0f, maxSpeed);
+        rig.velocity = velocityVector;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(transform.position, transform.position + (Vector3.down *
+        groundRaycastDistance), Color.white);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        {
+            // read input
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (isOnGround)
+                {
+                    isJumping = true;
+                }
+            }
+        }
     }
 }
