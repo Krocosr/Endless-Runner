2d9a4948d9d2b8c117b811706040aeb12ccd6ba7
Krocosr <ariopang@gmail.com>
2022-06-10 18:20:05 +0700

    Summary : Moving Character
    - add script to character moving
    - add physics material to reduce friction

---
+++ Assets/Script/CharacterMoveController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMoveController : MonoBehaviour
+{
+    public float moveAccel;
+    public float maxSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
